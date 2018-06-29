25115468a3b123e112dffbd7b5bbcab1c51397c6
Austin Ballard <abelhawk@gmail.com>
2018-06-29 09:47:30 -0600

    Got firebase to work

--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -1,31 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { CharactersService } from '../../app-services/characters.service';
-import { AngularFirestoreCollection } from "angularfire2/firestore";
-import { FirebaseListObservable } from "angularfire2/database-deprecated";
-
-export interface Character {
-  id: string;
-  name: string;
-  haircolor: string;
-  age: number;
-}
+import { Component } from '@angular/core';
+import { AngularFirestore } from "angularfire2/firestore";
+import { Observable } from "rxjs/index";
 
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
   styleUrls: ['./character-list.component.css']
 })
-export class CharacterListComponent implements OnInit {
-  characters: FirebaseListObservable<any[]>;
+export class CharacterListComponent {
+  public characters: Observable<any[]>;
 
-  constructor(
-    // private CharactersService,
-  ) { }
-
-  ngOnInit() {
+  constructor(db: AngularFirestore) {
+    this.characters = db.collection('/characters').valueChanges();
   }
 
-  goToCharacterPage(character){
-    console.log("Go.");
-  }
 }
